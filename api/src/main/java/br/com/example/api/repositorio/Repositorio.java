4dd4d58443f3e266da4e9a4cef4791349fa64c0a
Gessica Vasconcelos <143418965+Gessvi@users.noreply.github.com>
2025-03-20 10:42:55 -0300

    Delete api directory

--- api/src/main/java/br/com/example/api/repositorio/Repositorio.java
+++
@@ -1,9 +0,0 @@
-package br.com.example.api.repositorio;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.example.api.modelo.Pessoa;
-
-public interface Repositorio extends CrudRepository<Pessoa, Integer>{
-
-}
